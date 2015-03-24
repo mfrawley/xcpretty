7b774b86da59a5d29258b917ae57f96c3243f0f1
Delisa Mason <iskanamagus@gmail.com>
2015-03-23 22:12:00 -0700

    Clean snippets

--- lib/xcpretty/term.rb
+++ lib/xcpretty/term.rb
@@ -1,26 +1,21 @@
-
 module XCPretty
   class Term
 
-    COLOR_DETECTION_COMMAND = "tput colors"
-    LOCALE_DETECTION_COMMAND = "locale charmap"
-    UNICODE_LOCALE = "UTF-8"
+    COLOR_DETECTION_COMMAND = 'tput colors'
+    LOCALE_DETECTION_COMMAND = 'locale charmap'
+    UNICODE_LOCALE = 'UTF-8'
     MINIMUM_COLORS = 8
 
     def self.unicode?
-      begin
-        `#{LOCALE_DETECTION_COMMAND}`.strip == UNICODE_LOCALE
-      rescue
-        false
-      end 
+      `#{LOCALE_DETECTION_COMMAND}`.strip == UNICODE_LOCALE
+    rescue
+      false
     end
 
     def self.color?
-      begin
-        `#{COLOR_DETECTION_COMMAND}`.to_i >= MINIMUM_COLORS
-      rescue
-        false
-      end
+      `#{COLOR_DETECTION_COMMAND}`.to_i >= MINIMUM_COLORS
+    rescue
+      false
     end
   end
 end
